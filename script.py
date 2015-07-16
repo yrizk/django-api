99b7a8436fc23757fe7681db0fd961ce6374eb12
yrizk <yohanna.rizk@buzzfeed.com>
2015-07-16 11:30:02 -0400

    ready to starting making api reqs

--- script.py
+++ script.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 import sys
+import random
 
 if len(sys.argv) < 2: 
 	print 'Usage: python script.py <count>'
 	quit()
 
 count = sys.argv[1];
-print 'count : ' + str(count)
+print 'Making ' + count + ' heart requests'
+
+for i in range(0,int(count)):
+	rand_sec = random.randint(1,180) # 3 minute videos
+	rand_video_id = random.randint(1,3) # just 3 videos 
+	print 'rand_sec: ' + str(rand_sec) + ' rand_video_id: ' + str(rand_video_id)
+
+
+
+
