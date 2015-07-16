76d95e9adb825cb61c349e8971b7892619ab07f6
yrizk <yohanna.rizk@buzzfeed.com>
2015-07-16 10:09:54 -0400

    adding view for api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Heart,Video
+from django.http import HttpResponse 
 # Create your views here.
+
+def heart(request,video_id,hearted_time): 
+	try: 
+		video = Video.objects.get(id=video_id)
+		Heart.objects.create(video=video,sec=hearted_time)
+	except Video.DoesNotExist:
+		pass 
+	return HttpResponse('OK')
